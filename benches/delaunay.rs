92819339af87168a9a72c3b4a5a50f019c084071
dyzdyz010 <dyzdyz010@gmail.com>
2025-04-15 23:04:42 +0800

    Update dependencies and optimize Delaunay benchmark
    - Added `rayon` dependency for parallel processing in the Delaunay triangulation benchmark.
    - Updated `Cargo.toml` to include `rayon` version 1.10.
    - Modified the benchmark to utilize parallel iteration for point generation.
    - Adjusted benchmark iterations for improved performance testing.

--- benches/delaunay.rs
+++ benches/delaunay.rs
@@ -1,11 +1,13 @@
 use criterion::{criterion_group, criterion_main, Bencher, BenchmarkId, Criterion};
 use egui::Pos2;
+use rayon::iter::{IntoParallelIterator, ParallelIterator};
 use std::hint::black_box;
 
 use eframe_template::delaunay::triangulate;
 
 fn create_points(size: usize) -> Vec<Pos2> {
     (0..size)
+        .into_par_iter()
         .map(|_| Pos2::new(rand::random(), rand::random()))
         .collect()
 }
@@ -22,10 +24,10 @@ fn create_benchmark_fn(b: &mut Bencher, size: usize) {
 fn criterion_benchmark(c: &mut Criterion) {
     let mut group = c.benchmark_group("Delaunay Triangulation");
     let iterations: [usize; 8] = [
-        100, 500, 1_000, 10_000, 100_000, 500_000, 1_000_000, 2_000_000,
+        500, 500, 1_000, 10_000, 100_000, 500_000, 1_000_000, 2_000_000,
     ];
 
-    for size in iterations.iter().take(3) {
+    for size in iterations.iter().take(1) {
         group.bench_with_input(
             BenchmarkId::new("delauney2d", size),
             size,
